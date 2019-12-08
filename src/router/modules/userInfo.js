41416949e281f8e3c0f10cb77be54d9529a61641
qingjiang.li@ninebot.com <qing_jiang_li@163.com>
2019-12-08 12:13:38 +0800

    init project

---
+++ src/router/modules/userInfo.js
@@ -0,0 +1,81 @@
+/** When your routing table is too long, you can split it into small modules **/
+
+import Layout from '@/layout'
+
+const userRouter = {
+  path: '/user',
+  component: Layout,
+  redirect: '/user/user/infomation',
+  name: 'UserInfo',
+  meta: {
+    title: '用户',
+    icon: 'nested'
+  },
+  children: [
+    {
+      path: 'infomation',
+      component: () => import('@/views/user/infomation/infomation1'),
+      name: '用户信息',
+      redirect: '/user/infomation',
+      meta: { title: '用户信息' },
+      children: [
+        {
+          path: 'infomation1',
+          component: () => import('@/views/user/infomation/infomation1'),
+          name: '用户信息1',
+          meta: { title: '用户信息1' }
+        },
+        {
+          path: 'infomation2',
+          component: () => import('@/views/user/infomation/infomation2'),
+          name: '用户信息2',
+          meta: { title: '用户信息2' }
+        }
+      ]
+    },
+    {
+      path: 'permission',
+      component: () => import('@/views/user/permission/permission1'),
+      name: '用户权限',
+      redirect: '/user/permission',
+      meta: { title: '用户权限' },
+      children: [
+        {
+          path: 'permission1',
+          component: () => import('@/views/user/permission/permission1'),
+          name: '用户权限1',
+          meta: { title: '用户权限1' }
+        },
+        {
+          path: 'permission2',
+          component: () => import('@/views/user/permission/permission2'),
+          name: '用户权限2',
+          meta: { title: '用户权限2' }
+        }
+      ]
+    },
+    {
+      path: 'order',
+      component: () => import('@/views/user/orders/order1'),
+      name: '用户订单',
+      redirect: '/user/orders',
+      meta: { title: '用户订单' },
+      children: [
+        {
+          path: 'order1',
+          component: () => import('@/views/user/orders/order1'),
+          name: '用户订单1',
+          meta: { title: '用户订单1' }
+        },
+        {
+          path: 'order2',
+          component: () => import('@/views/user/orders/order2'),
+          name: '用户订单2',
+          meta: { title: '用户订单2' }
+        }
+      ]
+    }
+  ]
+}
+
+export default userRouter
